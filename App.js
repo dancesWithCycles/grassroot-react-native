bea6f469c0eb42c48d078102648f94898f95f013
Stefan Name <sbegerad@posteo.de>
2021-03-25 15:24:06 -0400

    added start and destination points

--- App.js
+++ App.js
@@ -1,28 +1,67 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import RenderPoint from './point';
+import {lineString as makeLineString} from '@turf/helpers';
+import MapboxDirectionsFactory from '@mapbox/mapbox-sdk/services/directions';
 
+const accessToken='pk.eyJ1IjoiZGFuY2Vzd2l0aGN5Y2xlcyIsImEiOiJja205eXR5bzkxbW96Mndxc3p6dHV1c3Q3In0.lzOkgTEjmXQmmT_3ak_J2Q';
 MapboxGL.setAccessToken(
-    'pk.eyJ1IjoiZGFuY2Vzd2l0aGN5Y2xlcyIsImEiOiJja205eXR5bzkxbW96Mndxc3p6dHV1c3Q3In0.lzOkgTEjmXQmmT_3ak_J2Q',
+    accessToken,
 );
+
+/*{accessToken: 'MAPBOX_TOKEN_GOES_HERE'}*/
+const directionsClient = MapboxDirectionsFactory({
+    accessToken:`${accessToken}`
+});
 
 const App = () => {
     const [coordinates] = useState([-76.82878274, 39.20821176]);
+
+    const startingPoint = [3.3362400, 6.5790100];
+    const destinationPoint = [ 3.3750014, 6.5367877 ];
+
+    const renderAnnotations=()=>{
+	console.log('renderAnnotations...');
+	return (
+	    <>
+		<MapboxGL.PointAnnotation
+            key="startPointAnnotation"
+            id="startPointAnnotation"
+            coordinate={startingPoint}
+		> 
+		<View
+   	    style={styles.pointView}
+		/>
+		</MapboxGL.PointAnnotation>
+
+		<MapboxGL.PointAnnotation
+            key="destinationPointAnnotation"
+            id="destinationPointAnnotation"
+        coordinate={destinationPoint}> 
+		<View
+   	    style={styles.pointView}
+		/>
+	    </MapboxGL.PointAnnotation>
+	    </>
+	);
+    }
+
     /*TODO: What is the difference between prop zoomLevel for MapView and for Camera?*/
     return (
     <View style={styles.page}>
       <View style={styles.container}>
           <MapboxGL.MapView
-      style={styles.map}
-      zoomLevel={11}>
+	style={styles.map}
+	zoomLevel={16}
+	centerCoordinate={coordinates}
+	    >
             <MapboxGL.Camera
 	zoomLevel={8}
-	centerCoordinate={coordinates}
+	centerCoordinate={startingPoint}
 	annimationDuration={0}
 	    />
-          <MapboxGL.PointAnnotation coordinate={coordinates} id="Test" />
-	  {RenderPoint('pointAnnotation','pointAnnotation')}
+	    {renderAnnotations()}
+
       {/*TODO: How to pass array to a function or component? <RenderPoint
     id='pointAnnotation'
     key='pointAnnotation'/>*/}
@@ -44,6 +83,14 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+    pointView:{
+	height:30,
+	width:30,
+	backgroundColor:'#3f00ff',
+	borderRadius:50,
+	borderColor:'#fff',
+	borderWidth:3,
+    },
 });
 
 export default App;
