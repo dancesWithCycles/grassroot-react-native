7791a6a24576116b1568fb433d512f8f12d48280
Stefan Name <sbegerad@posteo.de>
2021-03-25 20:46:13 -0400

    added direction for point array

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import {lineString as makeLineString} from '@turf/helpers';
@@ -16,26 +16,53 @@ const directionsClient = MapboxDirectionsFactory({
 
 const App = () => {
     const [coordinates] = useState([-76.82878274, 39.20821176]);
+    const [route,setRoute]=useState(null);
 
     const startingPoint = [3.3362400, 6.5790100];
     const destinationPoint = [ 3.3750014, 6.5367877 ];
     const startDestinationPoints = [startingPoint,destinationPoint];
 
+
+    //fetch direction
+    useEffect(()=>{
+        // setting interval: similar to ComponentDidMount
+        const timer=setInterval(()=>{
+	    fetchRoute();
+        },10000);
+        // clearing interval: similar to ComponentWillUnmount
+        return ()=>clearInterval(timer);
+    })
+
+    const fetchRoute=async()=>{
+	console.log('fetchRoute...');
+	const reqOptions={
+	    waypoints:[
+		{coordinates:startingPoint},
+		{coordinates:destinationPoint},
+	    ],
+	    profile:'driving-traffic',
+	    geometries:'geojson',
+	};
+
+	const res=await directionsClient.getDirections(reqOptions).send();
+	const newRoute=makeLineString(res.body.routes[0].geometry.coordinates);
+	setRoute(newRoute);
+    };
+
     const renderAnnotations=()=>{
 	console.log('renderAnnotations...');
 	return (
-	    startDestinationPoints.map((point,index)=>{
-		console.log('index: '+index);
+	    startDestinationPoints.map((point,index)=>(
 		    <MapboxGL.PointAnnotation
-		key={`${index}-startPointAnnotation`}
-		id={`${index}-startPointAnnotation`}
+		key={`${index}-PointAnnotation`}
+		id={`${index}-PointAnnotation`}
 		coordinate={point}
 		    >
 		    <View
 		style={styles.pointView}
 		    />
 		</MapboxGL.PointAnnotation>
-	    })
+	    ))
 	);
     }
 
@@ -45,7 +72,7 @@ const App = () => {
       <View style={styles.container}>
           <MapboxGL.MapView
 	style={styles.map}
-	zoomLevel={16}
+	zoomLevel={11}
 	centerCoordinate={coordinates}
 	    >
             <MapboxGL.Camera
@@ -53,11 +80,22 @@ const App = () => {
 	centerCoordinate={startingPoint}
 	annimationDuration={0}
 	    />
-	    {renderAnnotations()}
-
-      {/*TODO: How to pass array to a function or component? <RenderPoint
+	{renderAnnotations()}
+	{/*TODO: How to pass array to a function or component? <RenderPoint
     id='pointAnnotation'
     key='pointAnnotation'/>*/}
+	{route && (
+		<MapboxGL.ShapeSource
+	    id='shapeSource'
+	    shape={route}
+		>
+		<MapboxGL.LineLayer
+	    id='lineLayer'
+	    style={{lineWidth: 5, lineJoin: 'bevel', lineColor: '#ff0000'}}
+		/>
+		</MapboxGL.ShapeSource>
+	)
+	}
         </MapboxGL.MapView>
       </View>
     </View>
